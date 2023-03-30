f7271f0fb0b8edf4fbcbf7e06309bd31b9ef9d6d
Gabriel Vainer WSL <vai9er@gmail.com>
2023-03-30 03:02:18 -0700

    cleaned refresh2

--- graphical.c
+++ graphical.c
@@ -32,6 +32,102 @@ double get_cpu_utilization() {
     return 1.0 - idleDelta / totalDelta;
 }
 
+// void refresh2(int samples, int tdelay){
+//     clear_screen();
+//     int j = 0;
+//     float prev_memory_used = 0;
+//     float memory_used[samples];
+//     for (int i = 0; i < samples; i++) {
+//         double cpu_utilization = 100 * get_cpu_utilization();
+//         printf("Number of samples: %d -- every %d secs\n", samples, tdelay);
+        
+//         //step 1: get system information
+//         struct sysinfo systemInfo;
+//         sysinfo(&systemInfo);
+    
+//         // Get total and used memory
+//         float memory_total = systemInfo.totalram;
+//         memory_used[i] = systemInfo.totalram - systemInfo.freeram;
+
+//         // Print memory usage in kilobytes
+//         printf("Memory usage: %.0f kilobytes\n", (memory_used[i] / 1024)/1024);
+//         printf("---------------------------------------\n");
+//         printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
+
+//         for (j = 0; j <= i; j++){
+//             printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB", memory_used[j] / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+
+//             float change = memory_used[j] - prev_memory_used;
+//             if (change == 0) {
+//                 printf(" |o %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+//             } else if (change > 0) {
+//                 int num_hashes = change / (memory_total / (samples*10));
+//                 printf(" |");
+//                 for (int k = 0; k < num_hashes; k++) {
+//                     printf("#");
+//                 }
+//                 printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+//             } else {
+//                 int num_hashes = -change / (memory_total / (samples*10));
+//                 printf(" |");
+//                 for (int k = 0; k < num_hashes; k++) {
+//                     printf("@");
+//                 }
+//                 printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+//             }
+
+//             prev_memory_used = memory_used[j];
+//         }
+
+//         // Print memory usage in kilobytes after
+//         for (j = i+1; j < samples; j++){
+//             printf("\n");
+//         }
+//         printf("total cpu use = %.2f%%\n", cpu_utilization);
+
+//         //print the rest of the information
+//         printUsers();
+        
+        
+//         printMachineInfo();
+        
+//         sleep(tdelay);
+//         clear_screen();
+//     }
+// }
+
+void printMemoryUtilization(int samples, float memory_used[], float memory_total, float prev_memory_used) {
+    printf("---------------------------------------\n");
+    printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
+
+    for (int j = 0; j < samples; j++){
+        struct sysinfo systemInfo;
+        sysinfo(&systemInfo);
+        printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB", memory_used[j] / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
+
+        float change = memory_used[j] - prev_memory_used;
+        if (change == 0) {
+            printf(" |o %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+        } else if (change > 0) {
+            int num_hashes = change / (memory_total / (samples*10));
+            printf(" |");
+            for (int k = 0; k < num_hashes; k++) {
+                printf("#");
+            }
+            printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+        } else {
+            int num_hashes = -change / (memory_total / (samples*10));
+            printf(" |");
+            for (int k = 0; k < num_hashes; k++) {
+                printf("@");
+            }
+            printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
+        }
+
+        prev_memory_used = memory_used[j];
+    }
+}
+
 void refresh2(int samples, int tdelay){
     clear_screen();
     int j = 0;
@@ -51,33 +147,8 @@ void refresh2(int samples, int tdelay){
 
         // Print memory usage in kilobytes
         printf("Memory usage: %.0f kilobytes\n", (memory_used[i] / 1024)/1024);
-        printf("---------------------------------------\n");
-        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot)\n");
 
-        for (j = 0; j <= i; j++){
-            printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB", memory_used[j] / (1024 * 1024 * 1024), memory_total / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024), (memory_total + systemInfo.totalswap) / (1024 * 1024 * 1024));
-
-            float change = memory_used[j] - prev_memory_used;
-            if (change == 0) {
-                printf(" |o %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
-            } else if (change > 0) {
-                int num_hashes = change / (memory_total / (samples*10));
-                printf(" |");
-                for (int k = 0; k < num_hashes; k++) {
-                    printf("#");
-                }
-                printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
-            } else {
-                int num_hashes = -change / (memory_total / (samples*10));
-                printf(" |");
-                for (int k = 0; k < num_hashes; k++) {
-                    printf("@");
-                }
-                printf("* %.2f (%.2f)\n", change / (1024 * 1024 * 1024), memory_used[j] / (1024 * 1024 * 1024));
-            }
-
-            prev_memory_used = memory_used[j];
-        }
+        printMemoryUtilization(i+1, memory_used, memory_total, prev_memory_used);
 
         // Print memory usage in kilobytes after
         for (j = i+1; j < samples; j++){
@@ -88,7 +159,6 @@ void refresh2(int samples, int tdelay){
         //print the rest of the information
         printUsers();
         
-        ///FINISH THIS PART
         
         printMachineInfo();
         
